d5181db4eae24b733940a67c268158c5d5a712e0
Mokosu-bit <mokosu755@gmail.com>
2022-04-07 19:56:42 +0900

    chore: bootstrapを導入 #9

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,6 +7,9 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
+// bootstrap
+import "bootstrap"
+import "../stylesheets/application.scss";
 
 Rails.start()
 Turbolinks.start()
