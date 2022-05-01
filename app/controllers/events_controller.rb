65c277d016c51dd6d6fcd3bcda000f8bb7b10802
Mokosu-bit <mokosu755@gmail.com>
2022-05-01 14:53:48 +0900

    chore: 不要なファイル・行の削除 #11

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,69 +1,46 @@
 class EventsController < ApplicationController
   before_action :set_event, only: %i[ show edit update destroy ]
 
-  # GET /events or /events.json
   def index
     @events = Event.all
   end
 
-  # GET /events/1 or /events/1.json
-  def show
-  end
+  def show; end
 
-  # GET /events/new
   def new
     @event = Event.new
   end
-
-  # GET /events/1/edit
-  def edit
-  end
-
-  # POST /events or /events.json
+  
   def create
     @event = Event.new(event_params)
 
-    respond_to do |format|
-      if @event.save
-        format.html { redirect_to event_url(@event), notice: "Event was successfully created." }
-        format.json { render :show, status: :created, location: @event }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @event.errors, status: :unprocessable_entity }
-      end
+    if @event.save
+      redirect_to event_path(@event), success: 'イベントを作成しました'
+    else
+      render :new
     end
   end
 
-  # PATCH/PUT /events/1 or /events/1.json
+  def edit; end
+
   def update
-    respond_to do |format|
-      if @event.update(event_params)
-        format.html { redirect_to event_url(@event), notice: "Event was successfully updated." }
-        format.json { render :show, status: :ok, location: @event }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @event.errors, status: :unprocessable_entity }
-      end
+    if @event.update(event_params)
+      redirect_to event_path(@event), success: 'イベント内容を更新しました'
+    else
+      render :new
     end
   end
 
-  # DELETE /events/1 or /events/1.json
   def destroy
     @event.destroy
-
-    respond_to do |format|
-      format.html { redirect_to events_url, notice: "Event was successfully destroyed." }
-      format.json { head :no_content }
-    end
+    redirect_to events_path, success: 'イベントを削除しました'
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_event
       @event = Event.find(params[:id])
     end
 
-    # Only allow a list of trusted parameters through.
     def event_params
       params.require(:event).permit(:title, :mode)
     end
