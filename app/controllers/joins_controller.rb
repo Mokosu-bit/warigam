e895dd82a59ddede55bb259069f4c45121cd03ee
Mokosu-bit <mokosu755@gmail.com>
2022-05-09 13:41:53 +0900

    feat: イベントへの参加機能を追加 #25

---
+++ app/controllers/joins_controller.rb
@@ -0,0 +1,13 @@
+class JoinsController < ApplicationController
+  def create
+    event = Event.find(params[:event_id])
+    current_user.join(event)
+    redirect_to event_path, success: '参加しました'
+  end
+
+  def destroy
+    event = Event.find(params[:event_id])
+    current_user.cancel(event)
+    redirect_to event, success: '参加をキャンセルしました'
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  resources :events
   root "welcome#index"
+  resources :events do
+    resource :join, only: %i[create destroy]
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   devise_for :users, controllers: {
     omniauth_callbacks: "omniauth_callbacks"
