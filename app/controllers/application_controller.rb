81cda10c4709d4bd631a80b0ae9d2b516996db11
Mokosu-bit <mokosu755@gmail.com>
2022-05-11 17:36:31 +0900

    feat: ログインを要求する #29

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,15 @@
 class ApplicationController < ActionController::Base
+  before_action :store_current_location, unless::devise_controller?
+  before_action :authenticate_user!
+
+
+  def after_sign_in_path_for(resource)
+    
+  end
+
+  private
+
+  def store_current_location
+    store_location_for(:user, request.url)
+  end
 end
