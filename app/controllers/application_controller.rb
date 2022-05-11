f71761cc89a2843c95bfbeae784c88504e6908ce
Mokosu-bit <mokosu755@gmail.com>
2022-05-11 18:36:38 +0900

    feat: ログイン要求前の画面に遷移 #29

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,15 +1,18 @@
 class ApplicationController < ActionController::Base
-  before_action :store_current_location, unless::devise_controller?
+  before_action :store_current_location, if: :storable_location?
   before_action :authenticate_user!
-
-
-  def after_sign_in_path_for(resource)
-    
-  end
 
   private
 
+  def storable_location?
+    !devise_controller?
+  end
+
   def store_current_location
     store_location_for(:user, request.url)
+  end
+
+  def after_sign_in_path_for(resource)
+    store_location_for(resource) || root_path
   end
 end
