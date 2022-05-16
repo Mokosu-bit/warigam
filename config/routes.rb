f99856c693e84e03a6b533b89dd1977577d4f231
Mokosu-bit <mokosu755@gmail.com>
2022-05-16 12:57:29 +0900

    feat: モードによってビューを切り替え #35

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root "welcome#index"
   resources :events do
+    resource :rooms, only: %i[show]
     resource :join, only: %i[create destroy]
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
