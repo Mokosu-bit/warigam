132fd07902df87d8513880362be10a8016983b73
Mokosu-bit <mokosu755@gmail.com>
2022-05-27 16:07:57 +0900

    feat: 各モードに金額を追加 #39

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -2,6 +2,6 @@ class RoomsController < ApplicationController
 
   def show
     @event = Event.find(params[:event_id])
-    @joining_users = @event.joining_users
+    @joined_users = @event.joining_users
   end
 end
