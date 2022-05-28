2725bcdd55b0989768c9d2dac8b5702abd6e9ea3
Mokosu-bit <mokosu755@gmail.com>
2022-05-29 01:49:25 +0900

    feat: 単位モード追加 #41

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -5,12 +5,40 @@ class RoomsController < ApplicationController
     @joined_users = @event.joining_users
     
     # ----------- 割振モード -----------
+    if @event.mode == "share"
 
     # ----------- 平等モード -----------
-    @equal_amount = @event.gamble.total_amount / @event.gamble.people_number
-    @selected_users = @joined_users.sample(1)
+    elsif @event.mode == "equal"
+      @equal_amount = @event.gamble.total_amount / @event.gamble.people_number
+      @selected_users = @joined_users.sample(@event.gamble.people_number)
 
     # ----------- 単位モード -----------
+    elsif @event.mode == "unit"
+      @unit = []
+      @selected_users = @joined_users.sample(@unit.length)
+      unit_s = @event.gamble.total_amount.to_s.chars
+      unit_i = unit_s.map(&:to_i).reverse
+      unit_i.each_with_index do |unit, index|
+        @unit << 
+        if index == 0
+          unit * 1
+        elsif index == 1
+          unit * 10
+        elsif index == 2
+          unit * 100
+        elsif index == 3
+          unit * 1000
+        elsif index == 4
+          unit * 10000
+        else
+          0
+        end
+      end
+
+    # ----------- モードなし -----------
+    else
+      redirect_to event_rooms_path
+    end
   end
 
   def mode
