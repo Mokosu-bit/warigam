278ee2ac16954e1fb190e5e33b905791f2568f16
Mokosu-bit <mokosu755@gmail.com>
2022-05-29 00:23:56 +0900

    feat: 平等モード追加 #41

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -3,5 +3,23 @@ class RoomsController < ApplicationController
   def show
     @event = Event.find(params[:event_id])
     @joined_users = @event.joining_users
+    
+    # ----------- 割振モード -----------
+
+    # ----------- 平等モード -----------
+    @equal_amount = @event.gamble.total_amount / @event.gamble.people_number
+    @selected_users = @joined_users.sample(1)
+
+    # ----------- 単位モード -----------
+  end
+
+  def mode
+    event = Event.find(params[:event_id])
+    if event.counts.zero?
+      event.update(counts: 1)
+      redirect_to event_rooms_path
+    else
+      redirect_to event_rooms_path
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   root "welcome#index"
   resources :events do
-    resource :rooms, only: %i[show]
+    resource :rooms, only: %i[show] do
+      collection do
+        post 'mode'
+      end
+    end
     resource :join, only: %i[create destroy]
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
