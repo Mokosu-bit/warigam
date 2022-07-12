18f3e6c17fc9124755bf6728331bea4d6dfbf90a
Mokosu-bit <mokosu755@gmail.com>
2022-07-13 03:43:05 +0900

    fix: JS再修正 #90

---
+++ app/javascript/packs/event.js
@@ -0,0 +1,9 @@
+document.addEventListener('turbolinks:load', window.viewChange = function viewChange() {
+  mode = document.getElementById("event_mode").value;
+  pn = document.getElementById("people_number");
+  if ( mode == 'equal' ) {
+    pn.style.display = "";
+  } else {
+    pn.style.display = "none";
+  }
+});
