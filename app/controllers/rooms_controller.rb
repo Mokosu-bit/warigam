bf39a107dfb11ab91475315adbdfac434cc2b0f6
Mokosu-bit <mokosu755@gmail.com>
2022-07-19 23:47:50 +0900

    chore: アニメーション実装コメントアウト #93

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -49,13 +49,13 @@ class RoomsController < ApplicationController
   def equal
     event = Event.find(params[:event_id])
     joined_users = event.joining_users
-    # if event.counts.zero?
-    #   event.update(counts: 1)
-    #   event.update(results: joined_users.sample(event.gamble.people_number))
-    #   redirect_to event_rooms_path
-    # else
-    #   redirect_to events_path
-    # end
+    if event.counts.zero?
+      event.update(counts: 1)
+      event.update(results: joined_users.sample(event.gamble.people_number))
+      redirect_to event_rooms_path
+    else
+      redirect_to events_path
+    end
   end
 
   def unit
--- app/javascript/packs/slot.js
+++ app/javascript/packs/slot.js
@@ -1,72 +1,72 @@
-var roulette;
+// var roulette;
 
-async function jsonGet() {
-  try {
+// async function jsonGet() {
+//   try {
 
-    const url = location.href;
-    const res = await axios.get(url + '.json');
-    // jsonを文字列化してparse
-    const items = JSON.parse(JSON.stringify(res.data));
-    const keyArray = Object.keys(items);
-    const users = []
-    keyArray.forEach(function(element){
-      var name = items[element].name;
-      let len = 4;
-      if(name.length > len) {
-        var name = name.substring(0, len)+'';
-      }
-      users.push(name);
-    });
-    // ルーレットを開始
-    function start() {
-      roulette = setInterval(function() {
-        var idx = users[Math.floor(Math.random() * users.length)]; 
-        // ルーレット
-        document.getElementById("roulette").innerHTML = idx;
-      }, 100);
-    }
-    start();
-  } catch (err) {
-    console.error(err);
-  }
-}
+//     const url = location.href;
+//     const res = await axios.get(url + '.json');
+//     // jsonを文字列化してparse
+//     const items = JSON.parse(JSON.stringify(res.data));
+//     const keyArray = Object.keys(items);
+//     const users = []
+//     keyArray.forEach(function(element){
+//       var name = items[element].name;
+//       let len = 4;
+//       if(name.length > len) {
+//         var name = name.substring(0, len)+'';
+//       }
+//       users.push(name);
+//     });
+//     // ルーレットを開始
+//     function start() {
+//       roulette = setInterval(function() {
+//         var idx = users[Math.floor(Math.random() * users.length)]; 
+//         // ルーレット
+//         document.getElementById("roulette").innerHTML = idx;
+//       }, 100);
+//     }
+//     start();
+//   } catch (err) {
+//     console.error(err);
+//   }
+// }
 
 
-// ルーレットを停止
-function stop() {
-  if(roulette) {
-    clearInterval(roulette);
-    console.log(document.getElementById("roulette").innerHTML);
-  }
-}
+// // ルーレットを停止
+// function stop() {
+//   if(roulette) {
+//     clearInterval(roulette);
+//     console.log(document.getElementById("roulette").innerHTML);
+//   }
+// }
 
-async function loop() {
-  try {
-    const url = location.href;
-    const res = await axios.get(url.replace('/rooms','.json'));
-    const items = JSON.parse(JSON.stringify(res.data));
-    const membersCounts = Object.values(items)[3];
-    // jsonを文字列化してparse
-    let count = 0;
-    const countUp = () => {
-      if(count < membersCounts) {
-        jsonGet();
-      }
-      stop();
-      count++;
-    }
-    const intervalId = setInterval(() => {
-      countUp();
-      if(count > membersCounts) {　
-        clearInterval(intervalId);　//intervalIdをclearIntervalで指定している
-      }}, 5000);
-  } catch (err) {
-    console.log(err);
-  }
-}
-document.addEventListener('turbolinks:load', function() {
-  // クリックイベントを登録
-  st.onclick = () => {
-    loop();
-  };
-})
+// async function loop() {
+//   try {
+//     const url = location.href;
+//     const res = await axios.get(url.replace('/rooms','.json'));
+//     const items = JSON.parse(JSON.stringify(res.data));
+//     const membersCounts = Object.values(items)[3];
+//     // jsonを文字列化してparse
+//     let count = 0;
+//     const countUp = () => {
+//       if(count < membersCounts) {
+//         jsonGet();
+//       }
+//       stop();
+//       count++;
+//     }
+//     const intervalId = setInterval(() => {
+//       countUp();
+//       if(count > membersCounts) {　
+//         clearInterval(intervalId);　//intervalIdをclearIntervalで指定している
+//       }}, 5000);
+//   } catch (err) {
+//     console.log(err);
+//   }
+// }
+// document.addEventListener('turbolinks:load', function() {
+//   // クリックイベントを登録
+//   st.onclick = () => {
+//     loop();
+//   };
+// })
