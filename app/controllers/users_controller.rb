ce5585577f86e3a9487df200f07ea38c44ac2e01
Mokosu-bit <mokosu755@gmail.com>
2022-07-22 14:22:21 +0900

    feat: kaminariでページネーションを適用 #97

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
   def show
     @user = User.find(current_user.id) 
     joined_events = @user.joining_events
-    @finished_events = joined_events.where(counts: 1) 
+    @finished_events = joined_events.where(counts: 1).page(params[:page]).per(5)
   end
 
   def edit; end
