3d071ebddd011a9150b96da99345bcabde46a0d9
Mokosu-bit <mokosu755@gmail.com>
2022-08-04 17:16:33 +0900

    feat: ユーザー詳細に累計金額を表示 #99

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,5 +3,6 @@ class UsersController < ApplicationController
     @user = User.find(current_user.id) 
     joined_events = @user.joining_events
     @finished_events = joined_events.where(counts: 1).order(events: :desc).page(params[:page]).per(5)
+    @payment_amount = joined_events.inject(0) { |sum, item| sum + item.rooms.find_by(user_id: @user).payment }
   end
 end
